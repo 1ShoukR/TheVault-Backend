82ccc28d3fb05224abfbd6c1fff4016194f1dca0
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-07-21 03:28:08 -0400

    created middleware

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
+// initial app setup
 const app: Express = express();
 const port = process.env.PORT;
+
+// middleware
+app.use(express.json());
+
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Express + TypeScript Server');
