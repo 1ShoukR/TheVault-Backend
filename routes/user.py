e903ec5c1d881d9d6548bc6e51bca2d0c2ab1995
Rahmin <rahminshoukoohi@gmail.com>
2023-07-22 22:06:10 -0400

    created a route that reads a user created

--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from models import db
+from models.user import User
 
 
 bp = Blueprint('user', __name__)
@@ -6,3 +8,17 @@ bp = Blueprint('user', __name__)
 @bp.route('/get-user')
 def get_user():
     return 'get user'
+
+
+
+@bp.route('/create', methods=['POST'])
+def create():
+    print('hello')
+    create_user = User(
+        username=request.form.get('username'),
+        password = request.form.get('password'),
+        email = request.form.get('email')
+    )
+    print('this is user', create_user)
+    return 'done'
+
