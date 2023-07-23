7cd8e6f919ac1a4f18837439c1c98dcb06448003
Rahmin <rahminshoukoohi@gmail.com>
2023-07-22 23:18:38 -0400

    created a route that creates a user"

--- routes/user.py
+++ routes/user.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request
 from models import db
-from models.user import User
+import models
+from utils.errorhandling import UserAlreadyExistsError, UserCreationError
+
 
 
 bp = Blueprint('user', __name__)
@@ -10,15 +12,29 @@ def get_user():
     return 'get user'
 
 
-
 @bp.route('/create', methods=['POST'])
 def create():
-    print('hello')
-    create_user = User(
-        username=request.form.get('username'),
-        password = request.form.get('password'),
-        email = request.form.get('email')
-    )
-    print('this is user', create_user)
-    return 'done'
-
+    username = request.form.get('username')
+    email = request.form.get('email')
+    
+    found_user = models.user.User.query.filter(
+        (models.user.User.username == username) | (models.user.User.email == email)
+    ).first()
+    
+    if found_user:
+        if found_user.username == username:
+            raise UserAlreadyExistsError('username', username)
+        elif found_user.email == email:
+            raise UserAlreadyExistsError('email', email)
+        
+    try:
+        create_user = models.user.User(
+            username=username,
+            password=request.form.get('password'),
+            email=email
+        )
+        db.session.add(create_user)
+        db.session.commit()
+        return 'done'
+    except Exception as e:\
+        raise UserCreationError(str(e))
