acf3c1f211098f7eb3560d3c7f1fb8bc2b5f6bf6
Rahmin <rahminshoukoohi@gmail.com>
2023-07-23 02:27:23 -0400

    renamed table to better represent what the first feature will be

--- models/stored_passwords.py
+++ models/stored_passwords.py
@@ -2,11 +2,10 @@ from . import db
 
 
 
-class StoredPasswords(db.Model): 
-    __tablename__ = 'stored_passwords'
+class StoredPasswordsAndEmails(db.Model): 
+    __tablename__ = 'stored_passwords_and_emails'
     password_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-    website_for_password = db.Column(db.String(200), nullable=False)
     email_for_password = db.Column(db.String(200), nullable=True)
     username_for_password = db.Column(db.String(200), nullable=True)
-    password = db.Column(db.String(120), nullable=False)
+    password_for_email = db.Column(db.String(120), nullable=False)
