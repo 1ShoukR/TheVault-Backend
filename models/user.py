98d60270b30792d09c9b65ed89a189c0d6f84e21
Rahmin <rahminshoukoohi@gmail.com>
2023-07-23 02:15:20 -0400

    created new model so that every user is a person

--- models/user.py
+++ models/user.py
@@ -1,9 +1,21 @@
 from . import db
 
+class Person(db.Model):
+    __tablename__ = 'person'
+    person_id:int = db.Column(db.Integer, primary_key=True)
+    first_name:str = db.Column(db.String(120), unique=False, nullable=False)
+    last_name:str = db.Column(db.String(120), unique=False, nullable=False)
+    address:str = db.Column(db.String(120), unique=False, nullable=True)
+    # Define the one-to-one relationship with User
+    user_id:int = db.Column(db.Integer, db.ForeignKey('user.user_id'), unique=True, nullable=True)
+    user = db.relationship('User', backref='person', uselist=False)
+
+
+
 class User(db.Model):
     __tablename__ = 'user'
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(200), unique=True, nullable=False)
+    user_id:int = db.Column(db.Integer, primary_key=True)
+    username:str = db.Column(db.String(80), unique=True, nullable=False)
+    password:str = db.Column(db.String(120), nullable=False)
+    email:str = db.Column(db.String(200), unique=True, nullable=False)
     stored_passwords = db.relationship('StoredPasswords', backref='user', lazy=True)
